38a94055d70b37e3095b6cbc70ba54cf4a8f7960
Nathan Geddis <nathangeddis.98@gmail.com>
2021-03-31 15:39:04 -0700

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
 		response.statusCode = 200;
 		response.setHeader('Content-Type', 'text/plain');
-		response.end(Hellow World\n\nCool!');
+		response.end('Hellow World\n\nCool t!');
 });
 
 server.listen(port, (err) => {
